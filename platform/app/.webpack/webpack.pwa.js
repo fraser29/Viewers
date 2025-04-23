7005eedc254de16fb4926aa1d3d8120bb03895b3
Fraser Callaghan <callaghan.fm@gmail.com>
2025-04-23 14:11:33 +0200

    add mode

--- platform/app/.webpack/webpack.pwa.js
+++ platform/app/.webpack/webpack.pwa.js
@@ -78,6 +78,10 @@ module.exports = (env, argv) => {
         // Hoisted Yarn Workspace Modules
         path.resolve(__dirname, '../../../node_modules'),
         SRC_DIR,
+        path.resolve(
+          __dirname,
+          '/home/fraser/DEV/KISPI/KISPI-URO-V3/kispi/kispi-modes/kispi-uro/node_modules'
+        ),
       ],
     },
     plugins: [
